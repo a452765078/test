1821e573fd7dd1788ff725278e4d1111aa210ea1
tanwanwen <t_nmrg@163.com>
2023-05-27 14:46:24 +0800

    add

--- git2-b.js
+++ git2-b.js
@@ -1,1 +1,15 @@
 var two = 123;
+
+function study() {
+    return "I will study a new skill,hope you help me"
+}
+
+class Base {
+    page = 0;
+    pageSize = 10;
+    total = null
+    constructor(page=0,pageSize=10) {
+        this.page = page
+        this.pageSize = 10
+    }
+}
--- git2-c.js
+++ git2-c.js
@@ -2,3 +2,10 @@
 //   return 1;
 // };
 var d = 123;
+
+function sum(a,b) {
+    return a + b
+}
+function minus(a,b) {
+    return a - b
+}
